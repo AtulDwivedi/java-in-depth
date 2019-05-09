c841f8b49f54b920f96c2c37ac5c74c3aa65e912
Atul Dwivedi <atul16dwivedi@gmail.com>
2019-05-09 21:48:35 +0530

    single responsibility principle

---
+++ design/src/main/java/com/atuldwivedi/jid/principle/srp/good/HourReporter.java
@@ -0,0 +1,8 @@
+package com.atuldwivedi.jid.principle.srp.good;
+
+public class HourReporter {
+    //matters a lot to CFO
+    public String reportHours() {
+        return "Employee worked for 8 hours and paid 100$";
+    }
+}
---
+++ design/src/main/java/com/atuldwivedi/jid/principle/srp/good/PayCalculator.java
@@ -0,0 +1,12 @@
+package com.atuldwivedi.jid.principle.srp.good;
+
+import java.math.BigDecimal;
+
+public class PayCalculator {
+    //matters a lot to CFO
+    public BigDecimal calculatePay() {
+        // calculate pay of employee
+        // based on that employee’s contract, status, hours worked
+        return BigDecimal.TEN;
+    }
+}
